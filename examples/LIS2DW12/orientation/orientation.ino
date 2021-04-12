c337a2cfc74608d1025cd8e9f2b2409f5bc2b55b
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-04-12 15:53:22 +0800

    Update orientation.ino

--- examples/LIS2DW12/orientation/orientation.ino
+++ examples/LIS2DW12/orientation/orientation.ino
@@ -34,7 +34,7 @@ DFRobot_LIS2DW12_I2C acce;
 #elif defined(__AVR__) || defined(ARDUINO_SAM_ZERO)
 #define LIS2DW12_CS 3
 #elif (defined NRF5)
-#define LIS2DW12_CS 2  //The corresponding silkscreen on the development board is the pin of P2
+#define LIS2DW12_CS 2  //The pin on the development board with the corresponding silkscreen printed as P2
 #endif
 /*!
  * @brief Constructor 
