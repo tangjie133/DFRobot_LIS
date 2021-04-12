40fc9dabe1bd0646edebc5a55688d3ab7dc8dc51
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-04-12 15:47:37 +0800

    Update tapInterrupt.ino

--- examples/IIS2DLPC/tapInterrupt/tapInterrupt.ino
+++ examples/IIS2DLPC/tapInterrupt/tapInterrupt.ino
@@ -32,7 +32,7 @@ DFRobot_IIS2DLPC_I2C acce;
 #elif defined(__AVR__) || defined(ARDUINO_SAM_ZERO)
 #define IIS2DLPC_CS 3
 #elif (defined NRF5)
-#define IIS2DLPC_CS 2  //The corresponding silkscreen on the development board is the pin of P2
+#define IIS2DLPC_CS 2  //The pin on the development board with the corresponding silkscreen printed as P2
 #endif
 /*!
  * @brief Constructor 
@@ -157,7 +157,7 @@ void setup(void){
    Set the interval time between two taps when detecting double tap 
     dur duration(0 ~ 15)
     time = dur * (1/ODR)(unit:s)
-    |                                An example of a linear relationship between an argument and time                                                       |
+    |                            An example of a linear relationship between an argument and time                            |
     |------------------------------------------------------------------------------------------------------------------------|
     |                |                     |                          |                          |                           |
     |  Data rate     |       25 Hz         |         100 Hz           |          400 Hz          |         = 800 Hz          |
