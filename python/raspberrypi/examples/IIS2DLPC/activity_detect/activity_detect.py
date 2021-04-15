3ac085bf6eeb615a9f6125e21b16c26e6323ad16
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-04-15 16:06:31 +0800

    Update activity_detect.py

--- python/raspberrypi/examples/IIS2DLPC/activity_detect/activity_detect.py
+++ python/raspberrypi/examples/IIS2DLPC/activity_detect/activity_detect.py
@@ -2,12 +2,12 @@
 """
    @file activity_detect.py
    @brief Motion detection, can detect whether the module is moving
-   @n It’s necessary to go into low power mode before using this function. Then call setActMode() to make the chip in sleep mode. 
+   @n It’s necessary to go into low power mode before using this function. Then call setActMode() to make the chip enter sleep mode. 
    @n In this state, the measurement rate is 12.5hz.
    @n When the acceleration change in a certain direction is detected to exceed the threshold, the measurement rate will be increased to 
-   @n the set normal rate. The threshold can be set by the setWakeUpThreshold() function.
-   @n But if the move stops, that is, the change in acceleration in the three directions is less than the threshold, the chip will be in sleep 
-   @n mode after a period of time, which can be set by the setWakeUpDur() function.
+   @n the normal rate we set before. The threshold can be set by the setWakeUpThreshold() function.
+   @n But if the module stop moving, also, the change in acceleration in the three directions is less than the threshold, the chip will trun into sleep 
+   @n mode after a period of time. This duration time can be set by the setWakeUpDur() function.
    @n When using SPI, chip select pin can be modified by changing the value of RASPBERRY_PIN_CS
    @copyright  Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
    @licence     The MIT License (MIT)
@@ -23,11 +23,11 @@ sys.path.append("../../..") # set system path to top
 from DFRobot_LIS2DW12 import *
 import time
 
-#If you want to use SPI to drive this module, open the following two-line comments, and connect the module with Raspberry Pi via it
+#If you want to use SPI to drive this module, uncomment the codes below, and connect the module with Raspberry Pi via SPI port
 #RASPBERRY_PIN_CS =  27              #Chip selection pin when SPI is selected, use BCM coding method, the number is 27, corresponding to pin GPIO2
 #acce = DFRobot_IIS2DLPC_SPI(RASPBERRY_PIN_CS)
 
-#If you want to use I2C to drive this module, open the following three-line comments, and connect the module with Raspberry Pi via it
+#If you want to use I2C to drive this module, uncomment the codes below, and connect the module with Raspberry Pi via I2C port
 #The I2C address can be switched through the DIP switch (gravity version) or SDO pin (Breakout version) on the board
 I2C_BUS         = 0x01             #default use I2C1
 #ADDRESS_0       = 0x18             #sensor address 0
@@ -65,8 +65,8 @@ acce.set_filter_path(acce.LPF)
 acce.set_filter_bandwidth(acce.RATE_DIV_4)
 
 '''
-      Wake-up duration -- when the setActMode() function uses the detection mode of eDetectAct, it will be a period of time to collect data at a 
-     normal rate after the chip is awakened. Then the chip will continue to hibernate, collecting data at a frequency of 12.5hz.
+      Wake-up duration: when using the detection mode of eDetectAct in the setActMode() function, it will collect data
+    at a normal rate after the chip is awakened. Then after a period of time, the chip will continue to hibernate, collecting data at a frequency of 12.5hz.
      dur duration(0 ~ 3)
      time = dur * (1/rate)(unit:s)
      |                            An example of a linear relationship between an argument and time                            |
@@ -79,7 +79,7 @@ acce.set_filter_bandwidth(acce.RATE_DIV_4)
 '''
 acce.set_wakeup_dur(dur = 6)
 
-#Set wakeup threshold, when the acceleration variation exceeds this value, the eWakeUp event will be triggered,unit:mg
+#Set wakeup threshold, when the acceleration change exceeds this value, the eWakeUp event will be triggered,unit:mg
 #The value is within the range.
 acce.set_wakeup_threshold(0.05)
 
@@ -109,10 +109,9 @@ acce.set_power_mode(acce.CONT_LOWPWRLOWNOISE1_12BIT)
 '''
     Set the mode of motion detection:
                  NO_DETECTION      #No detection
-                 DETECT_ACT        #If set this mode, the rate of the chip will drop to 12.5hz and turn into normal measurement
-                                    frequency after the eWakeUp event is generated.
-                 DETECT_STATMOTION #In this mode, it can only detect if the chip is in sleep mode without changing the measurement 
-                                    frequency and power mode, continuously measuring the data at normal frequency.
+                 DETECT_ACT        #If set this mode, the rate of the chip will drop to 12.5hz and turn normal  after the eWakeUp event is generated.
+                 DETECT_STATMOTION #This mode can only detect if the chip is in sleep mode without changing the measurement frequency and power mode,
+                                    continuously measuring data at normal frequency.
 '''
 acce.set_act_mode(acce.DETECT_ACT)
 
